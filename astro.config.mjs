910983ac77de6ef409487fe1f127b8bad77f8fb1
nokynokynokynoky <drnodev@gmail.com>
2025-11-07 20:42:39 -0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+//export default defineConfig({});
+
+
+export default defineConfig({
+  site: 'https://nokynokynokynoky.github.io', // tu dominio principal
+  output: 'static', // modo estático (no necesitas adaptador)
+});
